9008350c9c1ffa8004a9c278b324075dbd039db3
Phat Tran <phat.tran85@live.com>
2013-10-01 16:14:30 +0700

    aaaaa

---
+++ CloudFileString/CustomUsernamePasswordAuth.Service/CalculatorDuplex.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel;
+using System.Text;
+
+namespace CustomUsernamePasswordAuth.Service
+{
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession, ConcurrencyMode = ConcurrencyMode.Reentrant)]
+    public class CalculatorDuplex : ICalculatorDuplex
+    {
+
+        // ++++++++++++++++++++++O+++++++++++++++++++++
+        ICalculatorDuplexCallback Callback = OperationContext.Current.GetCallbackChannel<ICalculatorDuplexCallback>();
+        double result = 0;
+        string equation;
+        public void AddTo(double n)
+        {
+            result += n;
+            equation += " + " + n.ToString();
+            Callback.NotifyEquals(result);
+        }
+    }
+}
---
+++ CloudFileString/CustomUsernamePasswordAuth.Service/ICalculatorDuplex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+
+namespace CustomUsernamePasswordAuth.Service
+{
+    [ServiceContract(SessionMode = SessionMode.Required, CallbackContract = typeof(ICalculatorDuplexCallback))]
+
+    public interface ICalculatorDuplex
+    {
+        [OperationContract(IsOneWay = true)]
+        void AddTo(double n);
+
+
+
+    }
+
+
+    public interface ICalculatorDuplexCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void NotifyEquals(double result);
+        [OperationContract(IsOneWay = true)]
+        void NotifyEquation(string eqn);
+    }
+
+}
